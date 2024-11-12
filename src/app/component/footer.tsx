aaf0371a0399b340065e6df4a834d5ce1471ed6b
Rao0898 <164119492+Rao0898@users.noreply.github.com>
2024-11-12 15:14:42 +0500

    Update footer.tsx

--- src/app/component/footer.tsx
+++ src/app/component/footer.tsx
@@ -3,7 +3,7 @@ import styles from './Footer.module.css'
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>&copy; 2023 Luxe Timepieces. All rights reserved.</p>
+      <p>&copy; 2023 Luxe Timepieces. All rights reserved. Made By Rao Ahmed</p>
     </footer>
   )
 }
